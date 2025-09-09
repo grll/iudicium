88f39109309adb9949afc0f996f0d2acc3f712eb
Guillaume Raille <guillaume.raille@icloud.com>
2025-09-10 00:18:29 +0300

    add working script for vastai

---
+++ scripts/run_vastai.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# install uv prior to running this script
+# curl -LsSf https://astral.sh/uv/install.sh | sh
+
+
+# inside the container, pick the interpreter that has torch preinstalled
+PY=$(command -v python)  # often /usr/bin/python or conda's python
+export UV_PYTHON="$PY"
+
+uv venv --system-site-packages
+uv sync --extra transformers # no --extra cpu/cu129 -> we don't install torch at all
+uv run python -m iudicium transformers_translator $@
