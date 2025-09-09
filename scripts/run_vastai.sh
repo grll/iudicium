796c8c79ca323a2f73f21820cc1b9c24f7a2c9ec
Guillaume Raille <guillaume.raille@icloud.com>
2025-09-10 00:19:40 +0300

    add doc

--- scripts/run_vastai.sh
+++ scripts/run_vastai.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
+# vastai is running in a docker container with already torch and cuda installed
+# this script reuse the system installed python env + cuda + torch
+# and install the rest.
 
 # install uv prior to running this script
 # curl -LsSf https://astral.sh/uv/install.sh | sh
-
 
 # inside the container, pick the interpreter that has torch preinstalled
 PY=$(command -v python)  # often /usr/bin/python or conda's python
